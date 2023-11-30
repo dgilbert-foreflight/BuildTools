8eaf802143435cfeb41e2a12550f6d59b5c24d59
Danny Gilbert <dgilbert@foreflight.com>
2023-11-30 10:59:46 -0600

    Example Project

---
+++ Projects/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Danny Gilbert on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
